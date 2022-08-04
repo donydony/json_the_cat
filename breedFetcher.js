6de47fec499237f1c2e88e7705face8cd0675cbf
Donny Zhao <donny.zh@hotmail.com>
2022-08-04 18:12:55 -0400

    linting

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const fetchBreedDescription = function (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (error) {
       callback(error, null);
